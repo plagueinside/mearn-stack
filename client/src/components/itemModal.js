6b5a075aab93b05b9953f195bd6c7892835928fb
Igor <kashubinia@gmail.com>
2018-10-01 18:17:19 +0300

    add delete_item

---
+++ client/src/components/itemModal.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import {
+    Button,
+    Modal,
+    ModalHeader,
+    Form,
+    ModalBody,
+    FormGroup,
+    Label,
+    Input
+} from 'reactstrap';
