e047365711613811c60d6a300dc8888d363ae8f5
Igor <kashubinia@gmail.com>
2018-10-02 14:38:19 +0300

    deploy update

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import uuid from 'uuid';
 import { connect } from 'react-redux';
 import { getItems, deleteItem } from '../actions/itemActions';
 import PropTypes from 'prop-types';
@@ -12,7 +11,7 @@ class ShoppingList extends Component {
     this.props.getItems();
   }
 
-  onDeleteClick = (id) => {
+  onDeleteClick = id => {
     this.props.deleteItem(id);
   }
 
@@ -22,14 +21,14 @@ class ShoppingList extends Component {
       <Container>
         <ListGroup>
           <TransitionGroup className="shopping-list">
-            {items.map(({ id, name }) => (
-              <CSSTransition key={id} timeout={500} classNames="fade">
+            {items.map(({ _id, name }) => (
+              <CSSTransition key={_id} timeout={500} classNames="fade">
                 <ListGroupItem>
                   <Button
                     className="remove-btn"
                     color="danger"
                     size="sm"
-                    onClick={this.onDeleteClick.bind(this, id)}
+                    onClick={this.onDeleteClick.bind(this, _id)}
                     >&times;</Button>
                   {name}
                 </ListGroupItem>
@@ -47,10 +46,7 @@ ShoppingList.propTypes = {
   item: PropTypes.object.isRequired
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   item: state.item
 });
-export default connect(
-  mapStateToProps,
-  { getItems, deleteItem }
-)(ShoppingList);
+export default connect(mapStateToProps,{ getItems, deleteItem })(ShoppingList);
