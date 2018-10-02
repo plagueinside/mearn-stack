dcf58e73c303f29d5c655a767670beca5376dec5
Igor <kashubinia@gmail.com>
2018-10-02 16:57:06 +0300

    change

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ mongoose.connect(db, {useNewUrlParser: true})
   .catch(err => console.log(err));
 
 app.use('/api/items', routes);
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
