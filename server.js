8cbdbaa6682026a30bbdc991d5c3d16c5527e78e
Igor <kashubinia@gmail.com>
2018-10-02 16:41:59 +0300

    set static port

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 
 // Serve static assets if in production
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
@@ -24,7 +24,7 @@ mongoose.connect(db, {useNewUrlParser: true})
   .catch(err => console.log(err));
 
 app.use('/api/items', routes);
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
